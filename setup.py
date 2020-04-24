95bac7b65ebf69e62c49d5da23c449f4eb947187
Florian Benkö <20401370+elcolumbio@users.noreply.github.com>
2020-04-24 15:23:08 +0200

    initial code from fastcounting

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cctable',
+      version='0.3',
+      description='Frontend visualizing accouting data.',
+      url='github.com/elcolumbio/cctable',
+      author='Florian Benkö',
+      author_email='f.benkoe@innotrade24.de',
+      license='Apache License, Version 2.0 (the "License")',
+      packages=['cctable'])
